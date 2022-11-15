cb3d69776f78d269a4a9f1d219f5663a085b4b12
Ha Young Roh <hyroh795@gmail.com>
2022-11-14 20:55:15 -0500

    commit tree 9f9a6e4eec74e7dbf3391e5fdf05d0b47cb040c9

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution here
+function findMatching(driverName, passedName) {
+    return driverName.filter(checkName);
+    function checkName(driverName) {
+        return driverName.toLowerCase() === passedName.toLowerCase();
+    }
+}
+
+
+
+function fuzzyMatch(driverName, passedLetters){
+    const matches = driverName.filter(s => s.startsWith(passedLetters));
+    return matches;
+
+}
+
+function matchName(arr, str) {
+    const matches = arr.filter(homeMatching);
+    function homeMatching (arr) {
+        return str === arr.name;
+    }
+    return matches;
+
+}
